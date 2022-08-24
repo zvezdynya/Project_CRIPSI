e8d315929758126513fec34e246d77d37eb7d279
zvezdynya <alya154@rambler.ru>
2022-08-24 17:57:27 +0300

    clone_project_cripsi

---
+++ README.md
@@ -0,0 +1,2 @@
+# frontend19_p2
+Frontend19 team project #2
---
+++ assets/js/about.js
@@ -0,0 +1,149 @@
+function readMore() {
+    let dots = document.getElementById("dots");
+    let more = document.getElementById("more");
+    let btn = document.getElementById("btn");
+    if (dots.style.display === "none") {
+        dots.style.display = "inline"
+        btn.innerHTML = "<img src='assets/images/plus.svg'>"
+        more.style.display = "none"
+    } else {
+        dots.style.display = "none"
+        btn.innerHTML = "<img src='assets/images/minus.svg'>"
+        more.style.display = "inline-block"
+    }
+}
+
+function readMore2() {
+    let dots2 = document.getElementById("dots2");
+    let more2 = document.getElementById("more2");
+    let btn2 = document.getElementById("btn2");
+    if (dots2.style.display === "none") {
+        dots2.style.display = "inline"
+        btn2.innerHTML = "<img src='assets/images/plus.svg'>"
+        more2.style.display = "none"
+    } else {
+        dots2.style.display = "none"
+        btn2.innerHTML = "<img src='assets/images/minus.svg'>"
+        more2.style.display = "inline-block"
+    }
+}
+
+function readMore3() {
+    let dots3 = document.getElementById("dots3");
+    let more3 = document.getElementById("more3");
+    let btn3 = document.getElementById("btn3");
+    if (dots3.style.display === "none") {
+        dots3.style.display = "inline"
+        btn3.innerHTML = "<img src='assets/images/plus.svg'>"
+        more3.style.display = "none"
+    } else {
+        dots3.style.display = "none"
+        btn3.innerHTML = "<img src='assets/images/minus.svg'>"
+        more3.style.display = "inline-block"
+    }
+}
+
+function readMore4() {
+    let dots4 = document.getElementById("dots4");
+    let more4 = document.getElementById("more4");
+    let btn4 = document.getElementById("btn4");
+    if (dots4.style.display === "none") {
+        dots4.style.display = "inline"
+        btn4.innerHTML = "<img src='assets/images/plus.svg'>"
+        more4.style.display = "none"
+    } else {
+        dots4.style.display = "none"
+        btn4.innerHTML = "<img src='assets/images/minus.svg'>"
+        more4.style.display = "inline-block"
+    }
+}
+
+function readMore5() {
+    let dots5 = document.getElementById("dots5");
+    let more5 = document.getElementById("more5");
+    let btn5 = document.getElementById("btn5");
+    if (dots5.style.display === "none") {
+        dots5.style.display = "inline"
+        btn5.innerHTML = "<img src='assets/images/plus.svg'>"
+        more5.style.display = "none"
+    } else {
+        dots5.style.display = "none"
+        btn5.innerHTML = "<img src='assets/images/minus.svg'>"
+        more5.style.display = "inline-block"
+    }
+}
+
+function readMore6() {
+    let dots6 = document.getElementById("dots6");
+    let more6 = document.getElementById("more6");
+    let btn6 = document.getElementById("btn6");
+    if (dots6.style.display === "none") {
+        dots6.style.display = "inline"
+        btn6.innerHTML = "<img src='assets/images/plus.svg'>"
+        more6.style.display = "none"
+    } else {
+        dots6.style.display = "none"
+        btn6.innerHTML = "<img src='assets/images/minus.svg'>"
+        more6.style.display = "inline-block"
+    }
+}
+
+function readMore7() {
+    let dots7 = document.getElementById("dots7");
+    let more7 = document.getElementById("more7");
+    let btn7 = document.getElementById("btn7");
+    if (dots7.style.display === "none") {
+        dots7.style.display = "inline"
+        btn7.innerHTML = "<img src='assets/images/plus.svg'>"
+        more7.style.display = "none"
+    } else {
+        dots7.style.display = "none"
+        btn7.innerHTML = "<img src='assets/images/minus.svg'>"
+        more7.style.display = "inline-block"
+    }
+}
+
+function readMore8() {
+    let dots8 = document.getElementById("dots8");
+    let more8 = document.getElementById("more8");
+    let btn8 = document.getElementById("btn8");
+    if (dots8.style.display === "none") {
+        dots8.style.display = "inline"
+        btn8.innerHTML = "<img src='assets/images/plus.svg'>"
+        more8.style.display = "none"
+    } else {
+        dots8.style.display = "none"
+        btn8.innerHTML = "<img src='assets/images/minus.svg'>"
+        more8.style.display = "inline-block"
+    }
+}
+
+function readMore9() {
+    let dots9 = document.getElementById("dots9");
+    let more9 = document.getElementById("more9");
+    let btn9 = document.getElementById("btn9");
+    if (dots9.style.display === "none") {
+        dots9.style.display = "inline"
+        btn9.innerHTML = "<img src='assets/images/plus.svg'>"
+        more9.style.display = "none"
+    } else {
+        dots9.style.display = "none"
+        btn9.innerHTML = "<img src='assets/images/minus.svg'>"
+        more9.style.display = "inline-block"
+    }
+}
+
+function readMore10() {
+    let dots10 = document.getElementById("dots10");
+    let more10 = document.getElementById("more10");
+    let btn10 = document.getElementById("btn10");
+    if (dots10.style.display === "none") {
+        dots10.style.display = "inline"
+        btn10.innerHTML = "<img src='assets/images/plus.svg'>"
+        more10.style.display = "none"
+    } else {
+        dots10.style.display = "none"
+        btn10.innerHTML = "<img src='assets/images/minus.svg'>"
+        more10.style.display = "inline-block"
+    }
+}
